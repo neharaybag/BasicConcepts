2d504d150e57da75a4e5558a49e8b94ff8522921
neha.raybagkar <neha.raybagkar@gmail.com>
2018-07-26 09:57:11 +0100

    Add project files.

---
+++ BasicConcepts/Classes/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicConcepts.Objects
+{
+    class Customer
+    {
+        private int customerId;
+        public string firstName; //public varibale can be accessed without get set
+        private string lastName;
+        private string location;
+        
+
+        public int CustomerId
+        {
+            get { return customerId; }
+            set { customerId = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+
+    }
+
+
+}
---
+++ BasicConcepts/Program.cs
@@ -0,0 +1,24 @@
+﻿using BasicConcepts.Objects;
+using System;
+
+namespace BasicConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is basic class createion, assigning and retreiving values
+            Customer customer = new Customer();
+            customer.firstName = "Test";
+            customer.CustomerId = 1234;
+            customer.LastName = "LastName";
+            customer.Location = "India";
+            Console.WriteLine("FirstName is :" + customer.firstName);
+            Console.WriteLine("LastName is :" + customer.LastName);
+            Console.WriteLine("Location is :" + customer.Location);
+            Console.WriteLine("Customer id  is :" + customer.CustomerId);
+            Console.ReadLine();
+
+        }
+    }
+}
